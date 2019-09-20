12cd79f397991a8f09192aa3e0dcd72bbad9e4b1
MacStudent <macstudent@126-iMac36.local>
2019-09-19 20:35:25 -0400

    pass or fail condition

---
+++ src/main/java/com/lambton/Airthmetic.java
@@ -0,0 +1,49 @@
+package com.lambton;
+
+public class Airthmetic
+{
+  int add(int a, int b)
+  {
+      int c;
+
+      c=a+b;
+
+      return c;
+
+
+  }
+    int sub(int a, int b)
+    {
+        int c;
+
+        c=a-b;
+
+        return c;
+
+
+    }
+    int multiply(int a, int b)
+    {
+        int c;
+
+        c=a*b;
+
+        return c;
+
+
+    }
+
+
+   float divide(int a, int b)
+
+    {
+        float c;
+
+        c=a/b;
+
+        return c;
+
+
+    }
+
+}
---
+++ src/main/java/com/lambton/CollegeExample.java
@@ -0,0 +1,40 @@
+package com.lambton;
+
+public class CollegeExample
+{
+
+    public static void main(String[] args)
+    {
+         int a,b;
+        Airthmetic airthmetic= new Airthmetic();
+
+
+      /*System.out.println("ADD :  " + airthmetic.add(21,12) );
+        System.out.println("SUB :  " + airthmetic.sub(21,12) );
+        System.out.println("MUlti " + airthmetic.multiply(21,12) );
+        System.out.println("DIVIDE :  " + airthmetic.divide(21,12) );*/
+
+        Student s1 = new Student();
+        s1.setFirstName("Camy");
+        s1.setLastName("Sandhu");
+
+        s1.setMarks1(40);
+        s1.setMarks2(40);
+        s1.setMarks3(50);
+        s1.setMarks4(50);
+        s1.setMarks5(50);
+        System.out.println(s1.getFullName());
+        s1.generateResult();;
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
